ca0fc5f5531673df8123627f1d1087012d9d7ae6
koray <koraycanpolat@hollandandbarrett.com>
2023-03-09 14:53:14 +0100

    more changes

--- playwright/pages/cornerstone/checkoutPage/checkout.ts
+++ playwright/pages/cornerstone/checkoutPage/checkout.ts
@@ -1,5 +1,5 @@
 import { Page, Locator } from "@playwright/test";
-import { BasePage } from "../../BasePage";
+import { BasePage } from "../../basePage";
 import * as data from "./checkout.data";
 import { faker } from "@faker-js/faker";
 
--- playwright/pages/cornerstone/homePage/home.ts
+++ playwright/pages/cornerstone/homePage/home.ts
@@ -1,5 +1,5 @@
 import { Page, Locator } from "@playwright/test";
-import { BasePage } from "../../BasePage";
+import { BasePage } from "../../basePage";
 import * as data from "./home.data";
 
 export class HomePage extends BasePage {
